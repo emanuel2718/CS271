30c375476ad8b8b7b3885e682872c4190a88c3b8
emanuel2718 <eramirez2718@gmail.com>
2020-05-08 22:03:41 +0100

    Push base instructions

---
+++ Program4.asm
@@ -0,0 +1,44 @@
+TITLE Program 4     (Program4.asm)
+
+; Author                : Emanuel Ramirez Alsina
+; Date Created          : 05/04/2020
+; Last Modified:        : 05/08/2020
+; OSU email address:    : ramieman@oregonstate.edu
+; Course number/section : 271/400
+; Project Number        : 04
+; Due Date:             : 05/10/2020
+; Description           : Program to calculate composite numbers. The number
+;                         must be in the range [1 .. 400]. The program will
+;                         display all the composite numbers up to the given
+;                         number.
+
+INCLUDE Irvine32.inc
+
+.const
+
+.data
+    ; Introduction, user information and instructions of the program variables
+    welcome         BYTE    "Composite Numbers   Programmed by Emanuel Ramirez", 0
+    programInfo     BYTE    "Enter the number of composites numbers you would "
+                    BYTE    "like to see.", 0
+    programInfo2    BYTE    "I'll accept orders for up to 400 composites", 0
+
+    promptNumber    BYTE    "Enter the number of composites [1 .. 400]", 0
+    
+    outOfRange      BYTE    "Out of range. Try again", 0
+
+    goodbye         BYTE    "Results certified by Emanuel Ramirez. Farewell", 0
+
+
+.code
+main PROC
+
+    mov     edx, OFFSET programInfo
+    call    WriteString
+    call    CRLF
+
+
+exit
+main ENDP
+
+END main

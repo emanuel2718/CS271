5b14425308fb9d5ef785b848293602a1a29dfa53
emanuel2718 <eramirez2718@gmail.com>
2020-05-24 04:47:41 +0100

    Fix introduction

--- Program5.asm
+++ Program5.asm
@@ -2,7 +2,7 @@ TITLE Program 5     (Program4.asm)
 
 ; Author                : Emanuel Ramirez Alsina
 ; Date Created          : 05/20/2020
-; Last Modified:        : 05/23/2020
+; Last Modified:        : 05/24/2020
 ; OSU email address:    : ramieman@oregonstate.edu
 ; Course number/section : 271/400
 ; Project Number        : 05
@@ -21,14 +21,15 @@ INCLUDE Irvine32.inc
 
 .data
     ; Introduction and program instructions
-    programTitle    BYTE    "Sorting and Counting Random Integers", 0
-    programmerName  BYTE    "                Programmed by Emanuel Ramirez", 0
-    programInfo     BYTE    "This program generates 200 random numbers in "
-                    BYTE    "the range [10 ... 29], displays the original "
-                    BYTE    "list, sorts the list, displays the median "
-                    BYTE    "value, displays the lsit sorted in ascending "
-                    BYTE    "order, then displays the number of instances "
-                    BYTE    "of each generated value", 0
+
+    programTitle    BYTE    "Program: Sorting and Counting Random Integers", 0
+    programmerName  BYTE    "Author: Emanuel Ramirez", 0
+    programInfo     BYTE    "Description: A program that generates 200 random"
+                    BYTE    " numbers in the range [10 ... 29]."
+                    BYTE    " It displays the list of random numbers and"
+                    BYTE    " then sorts in ascending order. Finally it"
+                    BYTE    " then displays the number of instances of each"
+                    BYTE    " generated value.", 0
 
 
 .code
@@ -37,7 +38,10 @@ INCLUDE Irvine32.inc
 ; Main procedure
 ; --------------------
 main PROC
+
+    call            Randomize
     call            introduction
+
     exit
 main ENDP
 
@@ -45,6 +49,7 @@ main ENDP
 introduction PROC
     mov             edx, OFFSET programTitle
     call            WriteString
+    call            CRLF
     mov             edx, OFFSET programmerName
     call            WriteString
     call            CRLF
